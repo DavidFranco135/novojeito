4a4e704a6428d5a598a5c67803ef3c859a984f9c
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-08 01:44:22 -0300

    Update Firebase configuration and add authentication

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-li6I0sC4pY34BPzVMbSRy3q51SFnkNA",
+  apiKey: "AIzaSyDx2p5tkMaeAjL6WrHNxFvK7rKZ77iFZ4w",
   authDomain: "financeiro-a7116.firebaseapp.com",
   projectId: "financeiro-a7116",
   storageBucket: "financeiro-a7116.firebasestorage.app",
   messagingSenderId: "952138197288",
-  appId: "1:952138197288:web:1ca7078badf9d8a5ce081d"
+  appId: "1:952138197288:web:11b60ce4a514d346ce081d"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
