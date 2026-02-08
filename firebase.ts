e4d49873cdb5248d2e5cf00460ecdad430e68486
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-08 02:37:51 -0300

    Import and export getStorage for Firebase

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { getStorage } from "firebase/storage"; // Importado
 
 const firebaseConfig = {
   apiKey: "AIzaSyDx2p5tkMaeAjL6WrHNxFvK7rKZ77iFZ4w",
@@ -15,4 +15,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app); // Adicionado para gerenciar fotos
+export const storage = getStorage(app); // Exportado para uso nas páginas
