485a831ddcb3b32f5db84a4eaa94e1c378d756e2
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-08 02:08:59 -0300

    Add Firebase Storage import to firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyDx2p5tkMaeAjL6WrHNxFvK7rKZ77iFZ4w",
   authDomain: "financeiro-a7116.firebaseapp.com",
