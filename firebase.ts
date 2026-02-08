65255e295b0b63abfd777f71075466c8e3a8aa20
DavidFranco135 <davidbhmg147@gmail.com>
2026-02-08 01:18:22 -0300

    Initialize Firestore in firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,5 +1,5 @@
-
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-li6I0sC4pY34BPzVMbSRy3q51SFnkNA",
@@ -11,3 +11,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
